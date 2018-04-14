80131a55376422fff8ac4bdcc5c5acc2c4b272e3
Beau Gunderson <beau@beaugunderson.com>
2018-04-13 22:39:24 -0700

    update for 2018 dataset

--- filter.sh
+++ filter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pv < subs.gz | gzcat | ./filter.js > distance-raw.txt
+pv < subs-2018.gz | gzcat | ./filter.js > distance-raw.txt
 
 sort distance-raw.txt > distance-sorted.txt
 sort distance-raw.txt | uniq -c  > distance-unique.txt
